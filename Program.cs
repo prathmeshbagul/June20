305f1a16756555a0c49b9610ed4ca1d18f98cc7c
Prathmesh <prathmesh.bagul@klingelnberg.com>
2022-06-20 18:03:21 +0530

    Built in Data Type

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace PieShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int monthlyWage=1234;
+            int months = 12; 
+            int bonus = 1000;
+
+            bool isActive = true;
+            double rating = 99.25;
+           // byte numberOfEmployees = 300;
+            Console.ReadLine();
+        }
+    }
+}
